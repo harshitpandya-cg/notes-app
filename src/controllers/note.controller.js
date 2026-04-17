d8a9689ac3ac5dcd1b1ab88e364446c56fcbfb7b
harshitpandya-cg <harshit.pandya.cg@gmail.com>
2026-04-17 23:17:41 +0530

    delete single note is working

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -206,3 +206,41 @@ exports.updateNote = async (req, res) => {
     });
   }
 };
+
+// 7. Delete single
+exports.deleteNote = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!isValidId(id)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid note ID",
+        data: null,
+      });
+    }
+
+    const note = await Note.findByIdAndDelete(id);
+
+    if (!note) {
+      return res.status(404).json({
+        success: false,
+        message: "Note not found",
+        data: null,
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Note deleted successfully",
+      data: null,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: null,
+    });
+  }
+};
+
